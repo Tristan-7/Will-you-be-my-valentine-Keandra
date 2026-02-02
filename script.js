392f30df51c6e45532517935224361d99e977fe6
Tristan-7 <138681883+Tristan-7@users.noreply.github.com>
2026-02-02 06:46:21 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const noBtn = document.getElementById("noBtn");
+const yesBtn = document.getElementById("yesBtn");
+const question = document.getElementById("question");
+
+noBtn.addEventListener("mouseover", () => {
+  const x = Math.random() * (window.innerWidth - noBtn.offsetWidth);
+  const y = Math.random() * (window.innerHeight - noBtn.offsetHeight);
+  noBtn.style.position = "absolute";
+  noBtn.style.left = `${x}px`;
+  noBtn.style.top = `${y}px`;
+});
+
+yesBtn.addEventListener("click", () => {
+  question.textContent = "YAY 💘 I knew you’d say yes, Keandra!";
+  noBtn.style.display = "none";
+  yesBtn.style.display = "none";
+});
